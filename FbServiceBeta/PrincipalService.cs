d6dee57b3390565e702d12928582e86ef4f447ca
RoselM <rosel.mz94@gmail.com>
2016-09-24 22:45:31 -0500

    Service with main class, without timer

---
+++ FbServiceBeta/PrincipalService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Facebook;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+namespace FbServiceBeta
+{
+    class PrincipalService
+    {
+        static void Main(string[] args)
+        {
+
+            ServiceHelper service = new ServiceHelper();
+            var client = new WebClient();
+            string accessToken = service.requestingToken(client);
+            var facebook_client = new FacebookClient(accessToken);
+            dynamic posts = service.requestingFacebook(facebook_client, "BorrachosVIP");
+            int PostLenght = posts.data.Count;
+            String message = "its empty";
+            for (int post_iterator = 0; post_iterator < PostLenght; post_iterator++)
+            {
+                String comment = "its empty";
+
+                message = posts.data[post_iterator].message;
+                Console.WriteLine(" # publicacion " + message);
+
+                int num_comments = posts.data[post_iterator].comments.data.Count;
+                for (int comment_iterador = 0; comment_iterador < num_comments; comment_iterador++) 
+                {
+                    comment = posts.data[post_iterator].comments.data[comment_iterador].message;
+                    Console.WriteLine(" - Comentario "  + comment);
+                }
+              
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ FbServiceBeta/ServiceHelper.cs
@@ -0,0 +1,34 @@
+﻿using Facebook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FbServiceBeta
+{
+    class ServiceHelper
+    {
+
+        private string app_id = "306602469705321";
+        private string app_secret = "c0895fd936fe83691d424f06b8c45581";
+
+        public String requestingToken(WebClient client) 
+        {
+            string oauthUrl = string.Format("https://graph.facebook.com/oauth/access_token?type=client_cred&client_id={0}&client_secret={1}", app_id, app_secret);
+            string accessToken = client.DownloadString(oauthUrl).Split('=')[1];
+            return accessToken;
+      
+        }
+
+        public dynamic requestingFacebook(FacebookClient facebook_client,string search)
+        {
+            string search_final = String.Format("{0}/posts?fields=message,comments", search);
+            dynamic posts = facebook_client.Get(search_final);
+            return posts;
+        }
+
+
+    }
+}
